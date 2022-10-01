2203a587727f2c3e64827f5805b4599f4421278e
circusfloor <942408787@qq.com>
2022-10-01 11:52:01 +0800

    git init

--- pie/draw_arc.py
+++ pie/draw_arc.py
@@ -1,10 +1,18 @@
 import pygame, sys
 from pygame.locals import *
 import math
+from random import randint
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption('Drawing Arcs')
+pos_x = 300
+pos_y = 150
+
+vel_x = 5
+vel_y = 5
+
 
 while True:
     for event in pygame.event.get():
@@ -13,12 +21,21 @@ while True:
 
     screen.fill((255, 255, 255))
 
-    # 绘制弧形
-    color = 100, 255, 200
-    position = 300, 150, 200, 200
-    start_angle = math.radians(0)
-    end_angle = math.radians(180)
-    width = 8
-    pygame.draw.arc(screen, color, position, start_angle, end_angle, width)
+    pos_x += vel_x
+    pos_y += vel_y
 
-    pygame.display.update()
+    if pos_x > 600 or pos_x < 0:
+        vel_x = - vel_x
+    if pos_y > 500 or pos_y < 0:
+        vel_y = - vel_y
+
+    # 绘制弧形
+    for i in range(100):
+        color = randint(0, 255), randint(0, 255), randint(0, 255)
+        position = pos_x, pos_y, 200, 200
+        start_angle = math.radians(0)
+        end_angle = math.radians(180)
+        width = 8
+        pygame.draw.arc(screen, color, position, start_angle, end_angle, width)
+
+        pygame.display.update()
