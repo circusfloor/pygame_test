82ce2af7c7a23f948368745b892e3a310505158d
circusfloor <942408787@qq.com>
2022-10-01 11:40:20 +0800

    git init

--- pie/draw_line.py
+++ pie/draw_line.py
@@ -1,24 +1,25 @@
 import pygame, sys
 from pygame.locals import *
+from random import randint
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption('Drawing Lines')
-pos_x = 300
-pos_y = 250
-vel_x = 2
-vel_y = 1
+
 
 while True:
     for event in pygame.event.get():
         if event.type in (QUIT, KEYDOWN):
             sys.exit()
 
-    screen.fill((0, 80, 0))
+    screen.fill((0, 0, 0))
 
     # 绘制线条
-    color = 100, 255, 200
-    width = 8
-    pygame.draw.line(screen, color, (100, 100), (500, 400), width)
-
-    pygame.display.update()
+    for i in range(200):
+        start_pos = (randint(0, 600), randint(0, 500))
+        end_pos = (randint(0, 600), randint(0, 500))
+        color = randint(0, 255), randint(0, 255), randint(0, 255)
+        width = randint(2, 5)
+        pygame.draw.line(screen, color, start_pos, end_pos, width)
+        pygame.display.update()
--- pie/draw_rect.py
+++ pie/draw_rect.py
@@ -1,13 +1,17 @@
 import pygame, sys
 from pygame.locals import *
+from random import randint
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption('Drawing Rectangles')
 pos_x = 300
 pos_y = 250
-vel_x = 2
-vel_y = 1
+vel_x = 0.2
+vel_y = 0.1
+color = 255, 255, 0
+
 while True:
     for event in pygame.event.get():
         if event.type in (QUIT, KEYDOWN):
@@ -21,12 +25,13 @@ while True:
 
     # 保持矩形在屏幕上
     if pos_x > 500 or pos_x < 0:
+        color = randint(0, 255), randint(0, 255), randint(0, 255)
         vel_x = - vel_x
     if pos_y > 400 or pos_y < 0:
+        color = randint(0, 255), randint(0, 255), randint(0, 255)
         vel_y = - vel_y
 
     # 绘制矩形
-    color = 255, 255, 0
     width = 0
     pos = pos_x, pos_y, 100, 100
     pygame.draw.rect(screen, color, pos, width)
