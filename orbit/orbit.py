82a8a37d7a5fbbcc91326d3b67150eb90a34c65e
circusfloor <942408787@qq.com>
2022-10-05 10:26:17 +0800

    git init

--- orbit/orbit.py
+++ orbit/orbit.py
@@ -2,10 +2,14 @@ import pygame, sys, math, random
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Orbit')
 
-space= pygame.image.load('space.png').convert()
+space = pygame.image.load('space.png').convert()
+planet = pygame.image.load('planet2.png').convert_alpha()
+ship = pygame.image.load('freelance.png').convert_alpha()
+width, height = ship.get_size()
+ship = pygame.transform.smoothscale(ship, (width//2, height//2))
 
 while True:
     for event in pygame.event.get():
@@ -18,4 +22,8 @@ while True:
 
     screen.blit(space, (0, 0))
 
+    width, height = planet.get_size()
+    screen.blit(planet, (400 - width / 2, 300 - height / 2))
+
+    screen.blit(ship, (50, 50))
     pygame.display.update()
