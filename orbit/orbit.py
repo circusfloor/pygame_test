d766a9fa1d1331894fb724fa68320b666cb0e126
circusfloor <942408787@qq.com>
2022-10-09 21:26:29 +0800

    git init

--- orbit/orbit.py
+++ orbit/orbit.py
@@ -2,6 +2,15 @@ import pygame, sys, math, random
 from pygame.locals import *
 
 
+def wrap_angle(angle):
+    return angle % 360
+
+
+def print_text(font, x, y, text, color=(255, 255, 255)):
+    imgtext = font.render(text, True, color)
+    screen.blit(imgtext, (x, y))
+
+
 class Point:
     def __init__(self, x, y):
         self._x = x
@@ -28,15 +37,24 @@ class Point:
             ",Y:" + ":.0f".format(self._y) + "}"
 
 
+# 游戏初始化
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Orbit')
+font = pygame.font.Font(None, 18)
 
+# 读取位图
 space = pygame.image.load('space.png').convert()
 planet = pygame.image.load('planet2.png').convert_alpha()
 ship = pygame.image.load('freelance.png').convert_alpha()
 width, height = ship.get_size()
 ship = pygame.transform.smoothscale(ship, (width//2, height//2))
+
+radius = 250
+angle = 0.0
+pos = Point(0, 0)
+old_pos = Point(0, 0)
+
 
 while True:
     for event in pygame.event.get():
@@ -52,5 +70,10 @@ while True:
     width, height = planet.get_size()
     screen.blit(planet, (400 - width / 2, 300 - height / 2))
 
-    screen.blit(ship, (50, 50))
+    angle = wrap_angle(angle - 0.1)
+    pos.x = math.sin(math.radians(angle)) * radius
+    pos.x = math.cos(math.radians(angle)) * radius
+    width, height = ship.get_size()
+    screen.blit(ship, (400+pos.x-width//2, 300+pos.y-height//2))
+
     pygame.display.update()
